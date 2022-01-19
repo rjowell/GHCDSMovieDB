3efa1bade931e955a75edc7af98a8f66989873de
rjowell20007 <44178512+rjowell@users.noreply.github.com>
2022-01-19 18:22:01 +0000

    added notes for console

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from movie import Movie
 
 movieList = [Movie("Action",120,4,"Rice",["Chris Evans","Henry Cavil","Margot Robbie","Zendaya"],"R",False),Movie("Comedy",116,1,"Elf",["Will Ferrell,Zoey Descahnel"],"PG",False),Movie("Children",125,1,"Toy Story",["Tom Hanks","Tim Allen"],"G",True),Movie("Action",113,1,"Die Hard",["Bruce Willis","Alan Rickman"],"R",False),Movie("Action",119,5,"Die Another Day",["Pierce Brosnan","Hallie Berry","Rosamund Pike","Judi Dench"],"PG-13",False)]
 
-print(len(movieList))
+'''
+Create a console app that says "Welcome to the movie database"
+What do you want to watch?
+A - Kids Movies (Rated PG and better)
+Action Movie
+Comedy Movies.
+Short Movies (movies < 75 minutes)
+
+
+'''
 
 
 
